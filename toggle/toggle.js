06564da7f5e96704ac72dfce7a4bb3e1da8f2d22
BrianN195 <brianick95@gmail.com>
2025-07-24 10:54:57 +0200

    jsImplentiert

---
+++ toggle/toggle.js
@@ -0,0 +1,5 @@
+function changeDirection () {
+    const toggleButton = document.getElementById ("toggleButton")
+    toggleButton.classList.toggle ("toggle-on")
+    toggleButton.classList.toggle ("toggle-off")
+}
